23d23b97c069740c27f96f3f1ce0f32559622bac
raddecke <ruben.dedecker@ugent.be>
2019-11-13 01:50:06 +0100

    fixed bugs and added automatic item fetching

--- src/QTOconverter.ts
+++ src/QTOconverter.ts
@@ -7,51 +7,53 @@ export class QTOconverter{
   private objectMap = new Map<any, any>();
   private graphMap = new Map<any, any>();
 
-  private graphsFromItem = new Map<any, any>();
-
   private objectPerId = new Map();
   private objectPerType = new Map();
 
   private processedIRIs = new Set();
 
+  ignoreTerms =  new Set(['termType', 'value', 'id', 'equals', 'datatype'])
 
   async processId(id : string){
     let baseId = id.split("#")
     if (! this.processedIRIs.has(baseId[0])){
-      console.log("processing file:", id)
       this.processQuads( (await this.fetcher.get(id)).triples )
       this.processedIRIs.add(baseId[0])
     }
+    return null;
   }
 
   private processQuads(quads : Array<any>){
     if (quads === null || quads === undefined || quads.length === undefined || quads.length === 0) {
       return;
     }
-
     let subjectMap = new Map<any, any>();
-    let ids = new Set<any>();
+    let nodeIds = new Set<any>();
   
     for (let quad of quads){
       if (quad !== undefined && quad !== null){
         this.addToListMap(subjectMap, this.getIdOrValue(quad.subject), quad)
-        if (quad.subject.termType === "NamedNode") {
-          ids.add(quad.subject.value)
+        if (quad.subject.termType === "NamedNode" || quad.subject.termType === "BlankNode"){
+          nodeIds.add(this.getIdOrValue(quad.subject))
         }
         if ( quad.graph.termType === "BlankNode" || quad.graph.termType === "NamedNode"){
           this.addToSetMap(this.graphMap, this.getIdOrValue(quad.graph), this.getIdOrValue(quad.subject))
         }
       }
     }
-    this.createObjects(subjectMap, ids);
+    this.createObjects(subjectMap, nodeIds);
+  }
+
+  async getItemForId(id : any){
+    if (! this.checkIdPresent(id)){
+      await this.processId(id)
+    }
+    return this.getConnectedBlankNodesForId(id)
+    // return this.objectPerId.get(id)
   }
 
   getReverseConnections(id : any){
     return this.objectMap.get(id)
-  }
-
-  getItemForId(id : any) : object{
-    return this.objectPerId.get(id)
   }
 
   checkIdPresent(id : any) : boolean {
@@ -73,73 +75,111 @@ export class QTOconverter{
   getAvailableIds() : Array<any>{
     return Array.from(this.objectPerId.keys())
   }
-
-  getAllItemsForGraph(graphId : any) : Array<any>{
-    let objectList = new Array()
-    for (let id of this.graphMap.get(graphId)){   
-      objectList.push(this.objectPerId.get(id))
-    }
-    return objectList
+  
+  getAvailableGraphIds() : Array<any>{
+    return Array.from(this.graphMap.keys())
   }
 
   checkItemContainedByGraph(itemId : any, graphId : any) : boolean{
     return this.graphMap.get(graphId).indexOf(itemId) !== -1
   }
 
-  getAllConnectedItemsForId(id : any){
-    let node = this.objectPerId.get(id)
-    return this._recursiveFillIds(node, new Array())
-
-  }
-  getAllConnectedItemsForNode(node : any){
-    return this._recursiveFillIds(node, new Array())
-
+  async getAllItemsForGraph(graphId : any) {
+    let objectList = new Array()
+    for (let id of this.graphMap.get(graphId)){   
+      objectList.push(await this.getItemForId(id))
+    }
+    return objectList
   }
 
-  ignoreTerms =  new Set(['termType', 'value', 'id', 'equals'])
-  private _recursiveFillIds(node : any, passedIds : Array<any>){
-    console.log(node)
+  async getConnectedBlankNodesForId(id : any){
+    return await this.getConnectedBlankNodesForNode(this.objectPerId.get(id))
+  }
+
+  async getConnectedBlankNodesForNode(node : any, passedIds : Array<any> = new Array()){
     let id = this.getIdOrValue(node);
     if (passedIds.indexOf(id) !== -1){
       return node;
+    } else {
+      passedIds.concat(this.getIdOrValue(node))
+    }
+    let returnNode = Object.assign(node)
+
+    if (this.graphMap.has(id)){
+      returnNode["@graph"] = await this.getAllItemsForGraph(id)
+      return returnNode
+    }
+
+    let nodeProperties = Object.keys(returnNode)
+    for (let property of nodeProperties){
+      if (! this.ignoreTerms.has(property)){
+        let propertyNodes = new Array()
+        for (let propertyNode of node[property]){
+          if (propertyNode.termType === "BlankNode"){
+            propertyNodes.push(await this.getConnectedBlankNodesForNode(propertyNode, passedIds))
+          } else {
+            propertyNodes.push(propertyNode)
+          }
+        }
+        returnNode[property] = propertyNodes
+      }
+    }
+    return returnNode
+  }
+
+  async getAllConnectedItemsForId(id : any){
+    let node = this.objectPerId.get(id)
+    return await this._recursiveFillIds(node, new Array())
+  }
+
+  async getAllConnectedItemsForNode(node : any){
+    return await this._recursiveFillIds(node, new Array())
+
+  }
+ 
+  private async _recursiveFillIds(node : any, passedIds : Array<any>) : Promise<any> {
+    let id = this.getIdOrValue(node);
+    if (passedIds.indexOf(id) !== -1){
+      return node;
+    } else {
+      passedIds = passedIds.concat(id)
     }
 
     if (this.checkGraphIdPresent(id)){
       let graphList = []
-      let graphIds = [this.getAllItemsForGraph(id)]
+      let graphIds = this.graphMap.get(id)
       for (let id of graphIds){
         let node = this.objectPerId.get(id)
-        graphList.push(this._recursiveFillIds(node, passedIds.concat(id)))
+        graphList.push(await this._recursiveFillIds(node, passedIds))
       }
+      return graphList
     }
     
     // Node is not a graph node
     if (this.checkIdPresent(id)){
-      node = this.getItemForId(id)
+      node = await this.getItemForId(id)
     }
     let nodeProperties = Object.keys(node)
     for (let property of nodeProperties){
-      let propertyNodes = new Array()
-      for (let propertyNode of property){
-        if (! this.ignoreTerms.has(property)){
-          propertyNodes.push(this._recursiveFillIds(propertyNode, passedIds.concat(id)))
+      if (! this.ignoreTerms.has(property)){
+        let propertyNodes = new Array()
+        for (let propertyNode of node[property]){
+          propertyNodes.push(await this._recursiveFillIds(propertyNode, passedIds))
         }
+        node[property] = propertyNodes
       }
-      node[property] = propertyNodes
     }
-
+    return node
   }
 
-  private createObjects(subjectMap : Map<any, any>, ids : Set<any>){
-    for (let id of Array.from(ids.keys())){
+  private createObjects(subjectMap : Map<any, any>, nodeIds : Set<any>){
+    for (let id of Array.from(nodeIds.keys())){
       let foundObj = this.searchMatching(id, id, subjectMap, new Set(), null)
-      if (! this.objectPerId.has(id)){
-        this.objectPerId.set(id, foundObj)
-        if (foundObj.hasOwnProperty("@type")){
-          this.addToMapMap(this.objectPerType, this.getIdOrValue(foundObj["@type"][0]), id, foundObj)
-        } else if (foundObj.hasOwnProperty('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')){
-          this.addToMapMap(this.objectPerType, this.getIdOrValue(foundObj['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'][0]), id, foundObj)
-        }
+      this.objectPerId.set(id, foundObj)
+      if (foundObj.hasOwnProperty("@type")){
+        this.addToMapMap(this.objectPerType, this.getIdOrValue(foundObj["@type"][0]), id, foundObj)
+      } else if (foundObj.hasOwnProperty('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')){
+        this.addToMapMap(this.objectPerType, this.getIdOrValue(foundObj['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'][0]), id, foundObj)
       }
     }
   }
@@ -168,30 +208,19 @@ export class QTOconverter{
             }
           }
           if (newaddition === true) {
-            if (quad.object.termType === "BlankNode") {
-              object[this.getIdOrValue(quad.predicate)].push(this.searchMatching(baseId, this.getIdOrValue(quad.object), subjectMap, visitedIds, quad.object));
-            } else {
-              if (quad.object.termType === "NamedNode") {
-                this.addToSetMap(this.objectMap, this.getIdOrValue(quad.object), baseId)
-              }
-              object[this.getIdOrValue(quad.predicate)].push(quad.object);
-            }
-          }
-        } else {
-          if (quad.object.termType === "BlankNode") {
-            object[this.getIdOrValue(quad.predicate)] = [ this.searchMatching(baseId, this.getIdOrValue(quad.object), subjectMap, visitedIds, quad.object) ];
-          } else {
-            if (quad.object.termType === "NamedNode") {
+            if (quad.object.termType === "NamedNode" || quad.object.termType === "BlankNode") {
               this.addToSetMap(this.objectMap, this.getIdOrValue(quad.object), baseId)
             }
-            object[this.getIdOrValue(quad.predicate)] = [ quad.object ];
+            object[this.getIdOrValue(quad.predicate)].push(quad.object);
           }
+        } else {
+          if (quad.object.termType === "NamedNode" || quad.object.termType === "BlankNode") {
+            this.addToSetMap(this.objectMap, this.getIdOrValue(quad.object), baseId)
+          }
+          object[this.getIdOrValue(quad.predicate)] = [ quad.object ];
         }
       }
 
-    } else if (this.graphMap.has(id)){
-      this.addToListMap(this.graphsFromItem, id, baseId)
-      return new Graph(id)
     } 
     return object; 
   }
@@ -204,13 +233,9 @@ export class QTOconverter{
       if (! object.hasOwnProperty("value")){ Object.defineProperty(object, "value", {value: object["id"], writable: true}) }
       return object["id"]
     } else {
-      throw new Error("Triple " + object.toString() + " contains no id or value field")
+      throw new Error("Triple " + object + " contains no id or value field")
     }
   }
-
-
-
-
 
   private addToListMap(listMap : Map<any, any>, key : any, value : any){
     if (listMap.has(key)){
